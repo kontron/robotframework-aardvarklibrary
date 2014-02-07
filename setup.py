72de87b339db87a3c942ac733a80687093b8b364
Michael Walle <michael.walle@kontron.com>
2014-02-07 15:23:50 +0100

    initial commit
    Signed-off-by: Michael Walle <michael.walle@kontron.com>

---
+++ setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+import sys
+sys.path.insert(0, 'src')
+
+def main():
+    setup(name = 'robotframework-aardvarklibrary',
+            version = '0.1',
+            description = 'Aardvark Library for Robot Framework',
+            author_email = 'michael.walle@kontron.com',
+            package_dir = { '' : 'src' },
+            license = 'Apache License 2.0',
+            classifiers = [
+                'Development Status :: 4 - Beta',
+                'License :: OSI Approved :: Apache Software License',
+                'Operating System :: OS Independent',
+                'Programming Language :: Python',
+                'Topic :: Software Development :: Testing',
+            ],
+            packages = [ 'AardvarkLibrary' ],
+            install_requires = [ 'robotframework', 'pyaardvark' ]
+    )
+
+if __name__ == '__main__':
+    main()
---
+++ src/AardvarkLibrary/utils.py
@@ -0,0 +1,32 @@
+from robot.utils import normalizing
+
+def _int_any_base(i, base=0):
+    try:
+        return int(i, base)
+    except TypeError:
+        return i
+
+def int_any_base(i, base=0):
+    try:
+        return _int_any_base(i, base)
+    except ValueError:
+        raise RuntimeError('Could not parse integer "%s"' % i)
+
+def list_any_input(data):
+    """Convert any input to list.
+
+    data can be:
+     basestring: '0x10 0x22 0xab' => [16, 32, 171]
+     list:        [16, 32, 171] => [16, 32, 171]
+     value:       16 => [16]
+     value:       0x10 => [16]
+    """
+    if isinstance(data, basestring):
+        data = [_int_any_base(d) for d in data.split(' ')]
+    elif isinstance(data, tuple):
+        data = [d for d in data]
+    elif isinstance(data, list):
+        data = data
+    else:
+        data = [_int_any_base(data)]
+    return data
