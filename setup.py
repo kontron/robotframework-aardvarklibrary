14930cfc13ba11c6bb5e9151db86e878b83f31de
Michael Walle <michael.walle@kontron.com>
2014-03-26 15:15:14 +0100

    setup.py: add build_libdoc command
    Use "python setup.py build_libdoc" to build the library documentation in
    docs/.
    
    Signed-off-by: Michael Walle <michael.walle@kontron.com>

--- setup.py
+++ setup.py
@@ -1,8 +1,26 @@
 #!/usr/bin/env python
 
-from setuptools import setup
+from setuptools import setup, Command
 import sys
 sys.path.insert(0, 'src')
+
+class run_build_libdoc(Command):
+    description = "Build Robot Framework library documentation"
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        try:
+            import robot.libdoc
+        except ImportError:
+            print "build_libdoc requires the Robot Framework package."
+            sys.exit(-1)
+        robot.libdoc.libdoc('AardvarkLibrary', 'docs/AardvarkLibrary.html')
 
 def main():
     setup(name = 'robotframework-aardvarklibrary',
@@ -22,7 +40,10 @@ def main():
             install_requires = [
                 'robotframework',
                 'pyaardvark >= 0.2',
-            ]
+            ],
+            cmdclass = {
+                'build_libdoc': run_build_libdoc,
+            },
     )
 
 if __name__ == '__main__':
